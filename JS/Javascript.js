024fe42370fdf855913e2d0d35e9910c3dc54910
andres <andres-jose1998@hotmail.com>
2021-12-16 20:54:15 -0500

    r

--- JS/Javascript.js
+++ JS/Javascript.js
@@ -51,8 +51,8 @@ function menu() {
 
 
 function openNav() {
-  document.getElementById("mySidenav").style.width = "250px";
-  document.getElementById("main").style.marginLeft = "250px";
+  document.getElementById("mySidenav").style.width = "350px";
+  document.getElementById("main").style.marginLeft = "350px";
   document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
